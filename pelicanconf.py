a66737a0e57215c70fc6dfaf74782f1aaf84b41e
saurabh-kumar88 <ykings.saurabh@gmail.com>
2023-05-02 00:05:52 +0530

    setup-done

---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+AUTHOR = 'saurabh'
+SITENAME = 'pelican-app-v1'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'UTC'
+
+DEFAULT_LANG = 'English'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('My Git hub', 'https://github.com/saurabh-kumar88'),
+          )
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
